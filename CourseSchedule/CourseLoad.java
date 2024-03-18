aaf66c242a62e3fabc946623b6bf5fc7488c4091
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-03-18 00:15:18 -0400

    Update CourseLoad.java

--- CourseSchedule/CourseLoad.java
+++ CourseSchedule/CourseLoad.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CourseSchedule;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author kal bugrara
- */
 public class CourseLoad {
     String semester;
     ArrayList<SeatAssignment> seatassignments;
