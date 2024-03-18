d9874b6399e0ab07429e5e7b420361ba1bb32c81
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-03-18 00:17:19 -0400

    Update Employment.java

--- Persona/EmploymentHistory/Employment.java
+++ Persona/EmploymentHistory/Employment.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Persona.EmploymentHistory;
 
 import CourseSchedule.CourseOffer;
 import Employer.EmployerProfile;
 import java.util.ArrayList;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class Employment {
     ArrayList<CourseOffer> relevantcourseoffers;
     int weight;
