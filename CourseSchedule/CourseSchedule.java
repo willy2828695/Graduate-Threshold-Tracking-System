2c13cafabab1c1f30aa2670f0c284c9f9fce28f7
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-03-18 00:15:47 -0400

    Update CourseSchedule.java

--- CourseSchedule/CourseSchedule.java
+++ CourseSchedule/CourseSchedule.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CourseSchedule;
 
 import CourseCatalog.Course;
 import CourseCatalog.CourseCatalog;
 import java.util.ArrayList;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class CourseSchedule {
 
     CourseCatalog coursecatalog;
