00af9846a34fee6067f9aeafddf323fc83034141
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-03-18 00:16:44 -0400

    Update DepartmentAccount.java

--- Department/DepartmentAccount.java
+++ Department/DepartmentAccount.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Department;
 
 import CourseSchedule.CourseSchedule;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class DepartmentAccount {
   Department department;
     public DepartmentAccount(Department d){
